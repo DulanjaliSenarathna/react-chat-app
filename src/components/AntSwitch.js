8de932a4ad170be861b603cf7743f48a8f793819
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-04-26 20:21:08 +0530

    Refactoring code in to re usable components

--- src/components/AntSwitch.js
+++ src/components/AntSwitch.js
@@ -5,43 +5,41 @@ const AntSwitch = styled(Switch)(({ theme }) => ({
   width: 40,
   height: 20,
   padding: 0,
-  display: "flex",
-  "&:active": {
-    "& .MuiSwitch-thumb": {
+  display: 'flex',
+  '&:active': {
+    '& .MuiSwitch-thumb': {
       width: 15,
     },
-    "& .MuiSwitch-switchBase.Mui-checked": {
-      transform: "translateX(9px)",
+    '& .MuiSwitch-switchBase.Mui-checked': {
+      transform: 'translateX(9px)',
     },
   },
-  "& .MuiSwitch-switchBase": {
+  '& .MuiSwitch-switchBase': {
     padding: 2,
-    "&.Mui-checked": {
-      transform: "translateX(20px)",
-      color: "#fff",
-      "& + .MuiSwitch-track": {
+    '&.Mui-checked': {
+      transform: 'translateX(20px)',
+      color: '#fff',
+      '& + .MuiSwitch-track': {
         opacity: 1,
-        backgroundColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.mode === 'dark' ? '#177ddc' : '#1890ff',
       },
     },
   },
-  "& .MuiSwitch-thumb": {
-    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+  '& .MuiSwitch-thumb': {
+    boxShadow: '0 2px 4px 0 rgb(0 35 11 / 20%)',
     width: 16,
     height: 16,
     borderRadius: 8,
-    transition: theme.transitions.create(["width"], {
+    transition: theme.transitions.create(['width'], {
       duration: 200,
     }),
   },
-  "& .MuiSwitch-track": {
+  '& .MuiSwitch-track': {
     borderRadius: 20 / 2,
     opacity: 1,
     backgroundColor:
-      theme.palette.mode === "dark"
-        ? "rgba(255,255,255,.35)"
-        : "rgba(0,0,0,.25)",
-    boxSizing: "border-box",
+      theme.palette.mode === 'dark' ? 'rgba(255,255,255,.35)' : 'rgba(0,0,0,.25)',
+    boxSizing: 'border-box',
   },
 }));
 
